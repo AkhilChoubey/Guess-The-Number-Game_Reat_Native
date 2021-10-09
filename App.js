d6493c6c711de5731dcf675354790b0d8d7a0652
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-10-10 00:12:02 +0530

    Added Header

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
-    <View>
-    
+    <View style={styles.screen}>
+    <Header title='Guess a Number' />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+      screen: {
+        flex: 1
+      }
 });
