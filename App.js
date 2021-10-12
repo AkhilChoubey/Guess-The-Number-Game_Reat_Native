f84a3ab1b17f2747d74cfb3228eb3a4ba1b8aa09
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-10-13 00:09:52 +0530

    Start game component

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from 'react-native/Libraries/NewAppScreen';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
     <Header title='Guess a Number' />
-      <StatusBar style="auto" />
+      <StartGameScreen />
     </View>
   );
 }
